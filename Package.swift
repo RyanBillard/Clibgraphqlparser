b0f78d77895001e3860737ff7cde5a240ec853e5
ryanbillard <ryan.billard@shopify.com>
2017-08-29 16:15:53 -0400

    back to 3

--- Package.swift
+++ Package.swift
@@ -1,13 +1,9 @@
-// swift-tools-version:4.0
+// swift-tools-version:3.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Clibgraphqlparser",
-    pkgConfig: "libgraphqlparser",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ]
+    pkgConfig: "libgraphqlparser"
 )
