0e4d57ec44973a9f72089e6dd3546c2f4052b4fd
ryanbillard <ryan.billard@shopify.com>
2017-08-29 16:03:14 -0400

    fix error in package file

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Clibgraphqlparser",
-    pkgConfig: "libgraphqlparser"
+    pkgConfig: "libgraphqlparser",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
